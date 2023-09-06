3ff68874fc7d4bd43b51e47e85805cec17449cc0
MarioSAJavier <115521090+MarioSAJavier@users.noreply.github.com>
2023-09-05 20:20:25 -0600

    Add files via upload

--- MachineLearning.py
+++ MachineLearning.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
@@ -11,6 +10,7 @@ class LogisticRegression():
         self.nIterations = nIterations
         self.weights = None
         self.bias = None
+        self.loss_history = []
 
     def fit(self, X, y):                             #Ajusta el modelo y por cada n iteracion cambiando el peso y el bias para la funcion sigmoide
         nSamples, nFeatures = X.shape
@@ -20,12 +20,14 @@ class LogisticRegression():
         for _ in range(self.nIterations):
             linearRegression = np.dot(X, self.weights) + self.bias
             predictions = sigmoid(linearRegression)
-
             dw = (1/nSamples) * np.dot(X.T, (predictions - y))
             db = (1/nSamples) * np.sum(predictions-y)
 
             self.weights = self.weights - self.learningRate*dw
             self.bias = self.bias - self.learningRate*db
+
+            loss = - (1 / nSamples) * np.sum(y * np.log(predictions) + (1 - y) * np.log(1 - predictions))
+            self.loss_history.append(loss)
 
 
     def predict(self, X):                              #Se hace la prediccion determinandolo por donde se encuentre el valor en la funcion sigmoide final obtenida del def init()
@@ -33,3 +35,6 @@ class LogisticRegression():
         yPredicted = sigmoid(linearRegression)
         classPrediction = [0 if y<=0.5 else 1 for y in yPredicted]
         return classPrediction
+
+    def get_loss_history(self):
+        return self.loss_history
